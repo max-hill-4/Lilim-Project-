47e287a34ee4d4aabfc6944a31dfa4c3e0eabd06
Max <maxwellhill2004@icloud.com>
2023-08-23 02:50:04 +0100

    API changes
    Stopped using Jinjas silly {{}} convention and moved to a more api approach where flask creates an endpoint to be used by javascript on the frontend

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import datahandler as dt
+import json
 
 app = Flask(__name__)
 
-data = {'bench' : dt.repMaxList('Bench Press'),'squat' : dt.repMaxList('Squat'), 'deadlift' : dt.repMaxList('Deadlift')}
+bench = {'data' : dt.repMaxList('Bench Press'), 'date' : dt.dateList('Bench Press')}
+squats = {'data' : dt.repMaxList('Squat'), 'date' : dt.dateList('Squat')}
+deadlift = {'data' : dt.repMaxList('Deadlift'), 'date' : dt.dateList('Deadlift')}
+
 
 @app.route('/')
 def index():
-    return render_template('index.html',data=data)
+    return render_template('index.html', bench=bench, squats=squats, deadlift=deadlift)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(bench)
--- datahandler.py
+++ datahandler.py
@@ -6,11 +6,9 @@ df = pd.read_csv('data.csv')
 def repMaxList(lift:str)-> list:
     filtered_df = df[df.iloc[:, 2] == lift]
     weightReps = filtered_df.iloc[:, 3:5].values.tolist()
-    date = filtered_df.iloc[:, 0].values.tolist()
     
     # Reverse the lists so that the newest data is first
     weightReps = weightReps[::-1]
-    date = date[::-1]
 
     
     formattedData = []
@@ -21,8 +19,17 @@ def repMaxList(lift:str)-> list:
 
     return formattedData
 
+def dateList(lift:str)-> list:
+    filtered_df = df[df.iloc[:, 2] == lift]
+    date = filtered_df.iloc[:, 0].values.tolist()
+    
+    # Reverse the lists so that the newest data is first
+    date = date[::-1]
+
+    return date
+
 def repMax(weightReps:list)->int:
     return (0.033 * weightReps[0] * weightReps[1]) + weightReps[0]
 
 
-print(repMaxList('Bench Press'))
+print(dateList('Bench Press'))
